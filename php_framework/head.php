56b9bbdb425f1ae0232196a492aa647d0909c296
tbouder <tbouder@student.42.fr>
2016-09-15 19:40:44 +0200

    Edit for correct path

--- php_framework/head.php
+++ php_framework/head.php
@@ -7,24 +7,22 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 09:10:31 by tbouder           #+#    #+#             */
-/*   Updated: 2016/09/15 13:25:32 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/09/15 19:35:11 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 	session_start();
-	function ft_head($camera = FALSE)
+
+	function ft_head()
 	{
 		echo '
 		<head>
 			<title>Camagru</title>
 			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 			<meta name="viewport" content="width=device-width" />
-			<link rel="stylesheet" type="text/css" href="./css/main.css" />
+			<link rel="stylesheet" type="text/css" href="'.CSS_DIR.'main.css" />
 			<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-			<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">';
-		// if ($camera == TRUE)
-			// echo '<script src="./take_picture.js"></script>';
-		echo '
+			<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">
 		</head>';
 	}
 ?>
--- php_framework/navbar.php
+++ php_framework/navbar.php
@@ -7,7 +7,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 09:10:31 by tbouder           #+#    #+#             */
-/*   Updated: 2016/09/14 19:09:53 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/09/15 19:40:03 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@
 		echo '
 		<div class="navbar_container">
 			<ul class="navbar_title_container">
-				<li class="navbar_title"><a href="index.php"><h1>Camagru</h1></a></li>
-				<li class="navbar_link"><a href="index.php"><h1>Acceuil</h1></a></li>
-				<li class="navbar_link"><a href="about.php"><h1>À propos</h1></a></li>';
+				<li class="navbar_title"><a href="'.PROJECT.'index.php"><h1>Camagru</h1></a></li>
+				<li class="navbar_link"><a href="'.PROJECT.'index.php"><h1>Home</h1></a></li>
+				<li class="navbar_link"><a href="'.PROJECT.'about.php"><h1>About</h1></a></li>';
 
 		if ($_SESSION["loggued_on_user"] == FALSE)
 		{
 			echo '
-				<li class="navbar_right"><a href="login.php"><h1>Se connecter</h1></a><br><br><a href="create_account.php"><h1>Créer un compte</h1></a></li>
+				<li class="navbar_right"><a href="'.PROJECT.'login.php"><h1>Login</h1></a><br><br><a href="'.PROJECT.'create_account.php"><h1>Create account</h1></a></li>
 			</ul>
 		</div>';
 		}
 		else
 		{
 			echo '
-				<li class="navbar_link navbar_right"><h1>'.$_SESSION['loggued_on_user'].'</h1><br><br><a href="logout.php" style="float:right"><h1>Se déconnecter</h1></a></li>
+				<li class="navbar_link navbar_right"><h1>'.$_SESSION['loggued_on_user'].'</h1><br><br><a href="'.PROJECT.'logout.php" style="float:right"><h1>Logout</h1></a></li>
 			</ul>
 		</div>';
 		}
